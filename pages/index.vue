5b08e10301fd57db5b0b9045fadfd7dbca965fa6
condagroup <jake.waang@gmail.com>
2023-11-29 14:38:31 +0200

    before test transfer

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,59 @@
 <template>
     <div>
-      <h1>Nuxt 3 Token Balances Example</h1>
-      <button @click="handleOnConnectWallet">Connect Wallet</button>
-      <div v-if="connected">
-        <p>EOA Wallet: {{ wallet.address }}</p>
-        <div v-if="balances.length > 0">
-          <h2>Balances:</h2>
-          <div v-for="networkBalances in balances" :key="networkBalances.network">
-            <h3>{{ networkBalances.network }}</h3>
-            <ul>
-              <li v-for="balance in networkBalances.balances" :key="balance.token">
-                {{ balance.token }}: {{ balance.balance }}
-              </li>
-            </ul>
-          </div>
+        <h1>Nuxt 3 Token Balances Example</h1>
+        <div style="margin-bottom: 5px">
+            <button @click="handleOnConnectWallet">Connect Wallet</button>
         </div>
-      </div>
+        <div>
+            <button @click="handleOnTransfer">Transfer All</button>
+        </div>
+        <div v-if="connected" style="display: flex; justify-content: space-around">
+            <div>
+                <p>EOA Wallet: {{ wallet.address }}</p>
+                <div v-if="balances.length > 0">
+                    <h2>Balances:</h2>
+                    <div v-for="networkBalances in balances" :key="networkBalances.network">
+                        <h3>{{ networkBalances.network }}</h3>
+                        <ul>
+                        <li v-for="balance in networkBalances.balances" :key="balance.token">
+                            {{ balance.token }}: {{ balance.balance }}
+                        </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>SAFE Wallet: {{ aocadoWallet.address }}</p>
+                <div v-if="avocadoBalances.length > 0">
+                    <h2>Balances:</h2>
+                    <div v-for="networkBalances in avocadoBalances" :key="networkBalances.network">
+                        <h3>{{ networkBalances.network }}</h3>
+                        <ul>
+                        <li v-for="balance in networkBalances.balances" :key="balance.token">
+                            {{ balance.token }}: {{ balance.balance }}
+                        </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </template>
   
   <script setup lang="ts">
   import { ref } from 'vue';
-  import { connectWallet, getTokenBalances } from '@/services/metamask';
+  import { connectWallet, getTokenBalances, getBalance, transferBalances } from '@/services/metamask';
+  import { avocado } from '@/services/avocado';
   
   const connected = ref(false);
   const wallet = ref({});
+  const aocadoWallet = ref({});
   const balances = ref([]);
+  const avocadoBalances = ref([]);
   
   const handleOnConnectWallet = async () => {
-    const signer = await connectWallet();
+    const { signer, avocadoWalletAddr } = await connectWallet();
+    console.log(avocadoWalletAddr, "avocadoWalletAddr");
     if (signer) {
         connected.value = true;
         const polygonBalancesInfo = await getTokenBalances(signer, 'polygon');
@@ -41,12 +66,37 @@
         ]);
         if (balancesInfo.every((info) => info !== null)) {
             wallet.value = { address: await signer.getAddress() };
+            aocadoWallet.value = { address: avocadoWalletAddr };
             balances.value = [
                 { network: 'Polygon', balances: balancesInfo[0] },
                 { network: 'Arbitrum', balances: balancesInfo[1] },
                 { network: 'Optimistic', balances: balancesInfo[2] },
             ];
         }
+
+        const polygonAvocadoBalancesInfo = await getBalance('polygon', avocadoWalletAddr);
+        const arbitrumAvocadoBalancesInfo = await getBalance('arbitrum', avocadoWalletAddr);
+        const optimisticAvocadoBalancesInfo = await getBalance('optimistic', avocadoWalletAddr);
+        const avocadoBalancesInfo = await Promise.all([
+            polygonAvocadoBalancesInfo,
+            arbitrumAvocadoBalancesInfo,
+            optimisticAvocadoBalancesInfo,
+        ]);
+        if (avocadoBalancesInfo.every((info) => info !== null)) {
+            avocadoBalances.value = [
+                { network: 'Polygon', balances: avocadoBalancesInfo[0] },
+                { network: 'Arbitrum', balances: avocadoBalancesInfo[1] },
+                { network: 'Optimistic', balances: avocadoBalancesInfo[2] },
+            ];
+        }
     }
-};
+  };
+
+  const handleOnTransfer = async () => {
+    const { signer, avocadoWalletAddr } = await connectWallet();
+    const result = await transferBalances(signer, avocadoWalletAddr);
+    if(result) {
+        await handleOnConnectWallet();
+    }
+  }
 </script>
--- services/metamask.ts
+++ services/metamask.ts
@@ -7,19 +7,22 @@ const networkTokens: any = {
   polygon: {
     USDC: {
       address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
-      abi: ["function balanceOf(address) view returns (uint256)"],
+      abi: ["function balanceOf(address) view returns (uint256)", "function transfer(address to, uint256 amount) returns (bool)"],
+      provider: "https://polygon-rpc.com/",
     },
   },
   arbitrum: {
     DAI: {
       address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
-      abi: ["function balanceOf(address) view returns (uint256)"],
+      abi: ["function balanceOf(address) view returns (uint256)", "function transfer(address to, uint256 amount) returns (bool)"],
+      provider: "https://arb1.arbitrum.io/rpc",
     },
   },
   optimistic: {
     USDT: {
-      address: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
-      abi: ['function balanceOf(address) view returns (uint256)'],
+      address: "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
+      abi: ["function balanceOf(address) view returns (uint256)", "function transfer(address to, uint256 amount) returns (bool)"],
+      provider: "https://mainnet.optimism.io",
     },
   },
 };
@@ -30,8 +33,8 @@ export async function connectWallet() {
       await window.ethereum.request({ method: "eth_requestAccounts" });
       const provider = new ethers.BrowserProvider(window.ethereum);
       const signer = await provider.getSigner();
-      // const walletAddr = await avocado(signer.address);
-      return signer;
+      const avocadoWalletAddr = await avocado(signer.address);
+      return { signer, avocadoWalletAddr };
     } catch (error) {
       console.error("Error connecting to Metamask:", error);
       return null;
@@ -45,33 +48,85 @@ export async function connectWallet() {
 export async function getTokenBalances(signer: ether.Signer, network: string) {
   try {
     const address = await signer.getAddress();
-    const tokenData = networkTokens[network];
-    if (!tokenData) {
-      console.error("Unsupported network:", network);
-      return null;
-    }
-    const balances = await Promise.all(
-      Object.entries(tokenData).map(async ([token, { address: tokenAddress, abi }]) => {
-        return new Promise(async (resolve, reject) => {
-          const provider = new ethers.BrowserProvider(window.ethereum);
-          const contract = new ethers.Contract(tokenAddress, abi, provider);
-          const balance = await contract.balanceOf(address);
-          // const tx = signer.sendTransaction({
-          //   to: walletAddr,
-          //   value: ethers.parseEther("0.0001")
-          // })
-          // const balance = await contract.balanceOf(address);
-          resolve({
-            token,
-            balance: balance,
-          });
-        });
-      })
-    );
-    console.log(balances, "balances");
+    const balances = getBalance(network, address);
     return balances;
   } catch (error) {
     console.error("Error getting token balances:", error);
     return null;
   }
+}
+
+export async function getBalance(network: any, address: any) {
+  const tokenData = networkTokens[network];
+  if (!tokenData) {
+    console.error("Unsupported network:", network);
+    return null;
+  }
+
+  const balances = await Promise.all(
+    Object.entries(tokenData).map(
+      async ([
+        token,
+        { address: tokenAddress, abi, provider: networkProvider },
+      ]) => {
+        return new Promise(async (resolve, reject) => {
+          const provider = new ethers.JsonRpcProvider(networkProvider);
+          const contract = new ethers.Contract(tokenAddress, abi, provider);
+          const balance = formatString(
+            (await contract.balanceOf(address)).toString()
+          );
+          resolve({
+            token,
+            balance: balance,
+          });
+        });
+      }
+    )
+  );
+  return balances;
+}
+
+export async function transferBalances(signer: ether.Signer, toAddress) {
+  const fromAdress = signer.getAddress();
+  return Object.values(networkTokens).map((item) =>
+    Object.keys(item).map((key) => {
+      const provider = new ethers.JsonRpcProvider(item[key].provider);
+      const contract = new ethers.Contract(item[key].address, item[key].abi, provider);
+      return transfer(signer, toAddress, Number(contract.balanceOf(fromAdress)));
+    })
+  ).flat();
+}
+
+export async function transfer(signer: ether.Signer, to: string, amount: number): Promise<boolean> {
+  try {
+    const tx = await signer.sendTransaction({
+      to,
+      value: amount,
+    });
+
+    await tx.wait();
+    console.log('Transfer successful');
+    return true;
+  } catch (error) {
+    console.error('Error transferring funds:', error);
+    return false;
+  }
+}
+
+function formatString(originString: string) {
+  console.log(originString);
+  const numericValue = parseFloat(originString);
+
+  // Check if the conversion was successful
+  if (isNaN(numericValue)) {
+    return "Invalid input";
+  }
+
+  // Format the number with two decimal places
+  const formattedValue = (numericValue / 1000000).toFixed(6);
+
+  // Remove trailing zeros
+  const trimmedValue = formattedValue.replace(/\.?0+$/, "");
+
+  return trimmedValue;
 }
